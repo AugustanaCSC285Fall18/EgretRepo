2eff467ce896ee426b556dc94419b615af60418e
Avery <mercedesvanopdorp14@augustana.edu>
2018-09-29 18:28:46 -0500

    Linked welcome window and editing window together.

--- ProjectEgret/src/edu/augustana/csc285/Egret/WelcomeWindow.java
+++ ProjectEgret/src/edu/augustana/csc285/Egret/WelcomeWindow.java
@@ -1,5 +1,31 @@
 package edu.augustana.csc285.Egret;
 
-public class WelcomeWindow {
+import org.opencv.core.Core;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class WelcomeWindow extends Application{
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("WelcomeWindow.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+			Scene scene = new Scene(root,root.getPrefWidth(),root.getPrefHeight());
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			//controller.initializeWithStage(primaryStage);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		launch(args);
+	}	
 }
+
--- ProjectEgret/src/edu/augustana/csc285/Egret/WelcomeWindowController.java
+++ ProjectEgret/src/edu/augustana/csc285/Egret/WelcomeWindowController.java
@@ -1,9 +1,15 @@
 package edu.augustana.csc285.Egret;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class WelcomeWindowController {
 
@@ -17,8 +23,16 @@ public class WelcomeWindowController {
     private Button newProjectBtn;
 
     @FXML
-    void createProject(ActionEvent event) {
-
+    void createProject(ActionEvent event) throws IOException{
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("EditingWindow.fxml"));
+		BorderPane root = (BorderPane)loader.load();
+//		EditingWindowController nextController = loader.getController();
+		
+		Scene nextScene = new Scene(root,root.getPrefWidth(),root.getPrefHeight());
+		nextScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		Stage primary = (Stage) newProjectBtn.getScene().getWindow();
+		primary.setScene(nextScene);
     }
 
     @FXML
