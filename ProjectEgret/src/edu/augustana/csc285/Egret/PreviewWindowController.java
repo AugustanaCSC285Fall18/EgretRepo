eb183486e5e23839a9a5ab0046c67b14cfcb7215
Avery <mercedesvanopdorp14@augustana.edu>
2018-10-19 12:08:58 -0500

    Edited preview window and added actions for window.

--- ProjectEgret/src/edu/augustana/csc285/Egret/PreviewWindowController.java
+++ ProjectEgret/src/edu/augustana/csc285/Egret/PreviewWindowController.java
@@ -1,54 +1,206 @@
 package edu.augustana.csc285.Egret;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
 public class PreviewWindowController {
 
     @FXML
-    private ImageView currentFrameImage;
+    private BorderPane browseBtn;
 
     @FXML
-    private Slider sliderSeekBar;
-
-    @FXML
-    private MenuItem returnOption;
+    private MenuItem advancedSettings;
 
     @FXML
     private MenuItem closeOption;
 
     @FXML
-    private MenuItem settingsOption;
+    private ImageView currentFrameImage;
 
     @FXML
-    private Button returnBtn;
+    private Canvas canvas;
 
     @FXML
-    private Button continueBtn;
+    private Slider sliderSeekBar;
 
     @FXML
-    void exitWindow(ActionEvent event) {
+    private Button loadBtn;
+
+    @FXML
+    private Button callibrateBtn;
+    
+    @FXML
+    private TextField startField;
+
+    @FXML
+    private TextField endField;
+    
+    private VideoCapture capture = new VideoCapture();
+	private String fileName = null;
+	private int curFrameNum;
+	public double numFrame;
+	
+	private double xCord;
+	private double yCord;
+	ProjectData data = new ProjectData();
+	private int animalCounter = 0;
+	private Video videoObject;
+	private GraphicsContext gc;
+
+	@FXML
+	public void initialize() {
+		sliderSeekBar.setDisable(true);
+		gc = canvas.getGraphicsContext2D();
+		runSliderSeekBar();
+	}
+	
+    @FXML
+    void handleBrowse(MouseEvent event) {
+    	FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Image File");
+		Window mainWindow = currentFrameImage.getScene().getWindow();
+		File chosenFile = fileChooser.showOpenDialog(mainWindow);
+		if (chosenFile != null) {
+			fileName = chosenFile.toURI().toString();
+			videoObject = new Video(fileName);
+			capture = videoObject.getVidCap();
+			startVideo();
+		};
+		runSliderSeekBar();
+		//runJumpTo(); //prints out which frame you are at
+    }
+    
+    protected void startVideo() {
+
+		// start the video capture
+		numFrame = this.capture.get(Videoio.CV_CAP_PROP_FRAME_COUNT);
+		//totalFrameArea.appendText("Total frames: " + (int) numFrame + "\n"); //prints total number of frames
+		sliderSeekBar.setDisable(false);
+		//this can be repurposed to allow the client to jump to specific time stamp
+		//jumpToFrameArea.setDisable(false); //allows client to jump to specific frame
+		updateFrameView();
+		sliderSeekBar.setMax((int) numFrame -1);
+		sliderSeekBar.setMaxWidth((int) numFrame -1);
+
+	}
+
+	/**
+	 * Get a frame from the opened video stream (if any)
+	 *
+	 * @return the {@link Mat} to show
+	 */
+	private Mat grabFrame() {
+		// init everything
+		Mat frame = new Mat();
+
+		// check if the capture is open
+		if (this.capture.isOpened()) {
+			try {
+				// read the current frame
+				this.capture.read(frame);
+
+				// if the frame is not empty, process it to black and white color
+				/*
+				 * if (!frame.empty()) { Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+				 * }
+				 */
+
+			} catch (Exception e) {
+				// log the error
+				System.err.println("Exception during the image elaboration: " + e);
+			}
+		}
+
+		return frame;
+	}
+
+	private void runSliderSeekBar() {
+
+		sliderSeekBar.valueProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				//currentFrameArea.appendText("Current frame: " + ((int) Math.round(newValue.doubleValue())) + "\n");
+
+				curFrameNum = (int) Math.round(newValue.doubleValue());
+				capture.set(Videoio.CAP_PROP_POS_FRAMES, curFrameNum);
+
+				updateFrameView();
+			}
+
+		});
+	}
+	
+
+//	private void runJumpTo() {
+//		
+//		jumpToFrameArea.textProperty().addListener(new ChangeListener<String>() {
+//			@Override
+//			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+//				int realValue = Integer.parseInt(newValue);
+//				currentFrameArea.appendText("Current frame: " + (realValue) + "\n");
+//				sliderSeekBar.setValue(realValue);
+//				curFrameNum = realValue;
+//				capture.set(Videoio.CAP_PROP_POS_FRAMES, curFrameNum);
+//				updateFrameView();
+//			}
+//
+
+//		});
+//
+//	}
+
+	public void updateFrameView() {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				// effectively grab and process a single frame
+				Mat frame = grabFrame();
+				// convert and show the frame
+				Image imageToShow = Utils.mat2Image(frame);
+				currentFrameImage.setImage(imageToShow);
+			}
+		});
+
+	}
+
+    @FXML
+    void handleCallibration(MouseEvent event) {
 
     }
 
     @FXML
-    void openExportWindow(MouseEvent event) {
+    void handleClose(ActionEvent event) {
 
     }
 
     @FXML
-    void openSettingsWindow(ActionEvent event) {
+    void handleLoadVideo(MouseEvent event) {
 
     }
 
     @FXML
-    void returnToPrevWindow(MouseEvent event) {
+    void handleSettings(ActionEvent event) {
 
+    }
+    
+    @FXML //throw exception if non number is entered.... or prevent it from being entered
+    void handleStartTime(KeyEvent event) {
+
+    }
+    
+    @FXML //throw exception if non number is entered.... or prevent it from being entered
+    void handleEndTime(KeyEvent event) {
+//    	int i = key.getKeyCode();
+//        if (i >= 65 && i <= 90)
+//        {
+//           ((TextField)event.getSource()).cancelKey();
+//        }
     }
 
 }
+
